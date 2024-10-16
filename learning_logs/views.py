2e485601f1d01c3849a1ce1c1dcd9f0088740d43
RichardMatheus929 <richard.ataliba.711@ufrn.edu.br>
2024-10-16 10:52:32 -0300

    startapp learning_logs

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from .models import Topic
 
 # Create your views here.
+class FirstView(CreateView):
+    model = Topic  # Especifica o modelo a ser criado
+    fields = ['title', 'text']  # Substitua pelos campos do modelo que deseja exibir
+    template_name = 'index.html'
+    success_url = '/'  # Redireciona após o sucesso
+
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        context['topics'] = Topic.objects.all()
+
+        return context
